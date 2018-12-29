b687d215fb2e318946dadd8a2aba042bad20b895
Alec Dusheck <a.dusheckirl@gmail.com>
2018-12-29 15:50:38 -0600

    Port file downloading/uploading over socket

--- src/manager/controllers/gameserver/helpers/socket.ts
+++ src/manager/controllers/gameserver/helpers/socket.ts
@@ -2,6 +2,11 @@ import {Helper} from "./helper";
 import {Gameserver} from "../gameserver";
 import {SSManager} from "../../../../ssmanager";
 
+import * as SocketIOStream from "socket.io-stream";
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as userid from "userid";
+
 class SocketHelper extends Helper {
 
     private websocket: any;
@@ -68,6 +73,78 @@ class SocketHelper extends Helper {
                 this.server.removeListener('installed', this.onInstalled);
                 this.server.removeListener('console', this.onConsole);
                 SSManager.logger.verbose("[Server " + this.server.id + " ] Socket disconnect");
+            });
+
+            //Download file event
+            SocketIOStream(socket).on('download', (stream, data) => {
+                if (!data.path) {
+                    return;
+                }
+                let filePath = this.server.fsHelper.extendPath(data.path);
+
+                if (filePath === "/home/" + this.server.id + "/public/identity.json") {
+                    return;
+                }
+
+                fs.stat(filePath, (err, stat) => {
+                    if (err) {
+                        socket.emit('fail', 'File does not exist.');
+                        socket.disconnect();
+                    }
+                    if (stat.isDirectory()) { //This is a dir ;/
+                        socket.emit('fail', 'Must be a file');
+                        socket.disconnect();
+                    } else if (!stat.isFile()) { //Lol wtf would this be?
+                        socket.emit('fail', 'Must be a file');
+                        socket.disconnect();
+                    }
+
+                    console.log("piping...");
+                    fs.createReadStream(filePath).pipe(stream);
+
+                    stream.on('close', () => {
+                        console.log("done!");
+                        socket.emit('status', 'done');
+                        socket.disconnect();
+                    });
+                });
+            });
+            //Upload file event
+            SocketIOStream(socket).on('upload', (stream, data) => { //TODO: check for possible file size limit bypass? I'm pretty sure the stream is generated server side.
+                if (!stream._readableState) { //Make sure we include the options
+                    socket.emit('fail', 'An error occurred.');
+                    socket.disconnect();
+                } else if (stream._readableState.highWaterMark > (SSManager.config.socket.maxFileSize * 1000000)) { //Make sure file size is under the config. 1000000b = 1mb
+                    socket.emit('fail', 'File too big');
+                    socket.disconnect();
+                }
+                if (!data.path || !data.name) {
+                    socket.emit('fail', 'Must specify path and name');
+                    socket.disconnect();
+                }
+                //Check to make sure the target isn't an existing folder
+                let filePath = this.server.fsHelper.extendPath(path.join(data.path, data.name)); //Almost forgot to extend the path after we joined hehe.
+                fs.stat(filePath).then(() => {
+
+                    if (filePath === "/home/" + this.server.id + "/public/identity.json") {
+                        socket.emit('fail', 'Restricted file target');
+                        socket.disconnect();
+                    }
+
+                    stream.pipe(fs.createWriteStream(filePath));
+                    fs.chown(filePath, userid.uid(this.server.id), userid.gid(this.server.id), () => {
+                        //Tell the manager we're done and disconnect
+                        socket.emit('status', 'done');
+                        socket.disconnect();
+                    });
+                }).catch(() => {
+                    stream.pipe(fs.createWriteStream(filePath));
+                    fs.chown(filePath, userid.uid(this.server.id), userid.gid(this.server.id), () => {
+                        //Tell the manager we're done and disconnect
+                        socket.emit('status', 'done');
+                        socket.disconnect();
+                    });
+                });
             });
 
         });
