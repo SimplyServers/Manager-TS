db71d7c630f217e3456c43965e2c405d4b31a8d1
Alec Dusheck <a.dusheckirl@gmail.com>
2019-01-08 22:07:51 -0600

    Log to file for debugging

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,23 +1,53 @@
+import {SSManager} from "../ssmanager";
+import * as path from 'path';
+import * as winston from 'winston';
+
 class Logger {
 
     //TODO: implement fs logging
     private readonly logToFile;
+    private logger;
 
     constructor(logToFile: boolean) {
         this.logToFile = logToFile;
+
+        const options = {
+            file: {
+                level: 'info',
+                filename: path.join(SSManager.getRoot(), "../app.log"),
+                handleExceptions: true,
+                json: true,
+                maxsize: 5242880, // 5MB
+                maxFiles: 5,
+                colorize: false,
+            },
+            console: {
+                level: 'debug',
+                handleExceptions: true,
+                json: false,
+                colorize: true,
+            },
+        };
+
+        this.logger = winston.createLogger({
+            transports: [
+                new winston.transports.File(options.file),
+                new winston.transports.Console(options.console)
+            ],
+            exitOnError: false
+        })
     }
 
     public info = (message: string) => {
-        console.log("[Info] " + message)
+        this.logger.info("[Info] " + message)
     };
 
     public verbose = (message: string) => {
-        if (process.env.NODE_ENV === 'dev')
-            console.debug("[Verbose] " + message)
+        this.logger.debug("[Verbose] " + message)
     };
 
     public error = (message: string) => {
-        console.error("[Error] " + message)
+        this.logger.error("[Verbose] " + message)
     };
 
 }
