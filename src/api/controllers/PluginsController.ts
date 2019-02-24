cba2ae8dfeefb52ee96bc2afb99123899aff6348
AlecDusheck <a.dusheckirl@gmail.com>
2019-02-24 17:54:57 -0600

    Fix typo in endpoint

--- src/api/controllers/PluginsController.ts
+++ src/api/controllers/PluginsController.ts
@@ -2,6 +2,6 @@ import {SSManager} from "../../SSManager";
 
 export class PluginsController {
     public getPlugins = async (req, res, next) => {
-        res.json({games: SSManager.configsController.plugins});
+        res.json({plugins: SSManager.configsController.plugins});
     };
 }
